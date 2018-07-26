127bc4314836a16aa2e62c21e089211328982357
Simon Levesque <simon@foilen.com>
2018-07-26 07:37:09 -0400

    typo

--- git-latest-version.sh
+++ git-latest-version.sh
@@ -6,7 +6,7 @@ set -e
 RUN_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $RUN_PATH
 
-# Clone all
+# Check all
 for project in $(cat projects.txt); do
   if [ -d "$project" ]; then
     echo $project: $(cd $project && git describe --abbrev=0 2> /dev/null )
