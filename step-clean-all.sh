8ebffe69f069451828275e528e621581053e0c94
Simon Levesque <simon@foilen.com>
2018-08-23 07:42:30 -0400

    better cleanup

--- step-clean-all.sh
+++ step-clean-all.sh
@@ -6,12 +6,16 @@ set -e
 RUN_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $RUN_PATH
 
-# Clone all
+# Clean all
 for project in $(cat projects.txt); do
   if [ -d "$project" ]; then
     echo "---[$project]---"
     cd $project
     ./step-clean.sh
+    if [ -d "bin" ]; then
+      rm -vrf bin
+    fi
+    find -type f | grep '\.class$' | xargs --no-run-if-empty rm -v
     cd ..
     echo
   fi
